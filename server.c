653c17f8be188136327265ec6abb625b7f67e89f
Mitchal42 <van8834@yandex.ru>
2015-01-25 02:02:53 +0300

    Создан сокет на сервере(Используем winsock2)

--- server.c
+++ server.c
@@ -1,5 +1,48 @@
+#include <stdio.h>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
 
-int main(int argv, char** argc)
+#pragma comment(lib, "ws2_32.lib")
+
+#define DEFAULT_PORT "27015"
+
+int main(int argc, char *argv[])
 {
-	   return 0;
+	WSADATA wsaData; //Никому не нужная переменная
+	int iResult;
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+	ZeroMemory(&hints, sizeof (hints));// Initialize to 0
+	hints.ai_family = AF_INET;//Internet
+	hints.ai_socktype = SOCK_STREAM;//Stream
+	hints.ai_protocol = IPPROTO_TCP;//TCP
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ListenSocket = INVALID_SOCKET;//new socket
+
+	// Create a SOCKET for the server to listen for client connections
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	return 0;
 }
